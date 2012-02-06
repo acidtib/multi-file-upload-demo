6d5560b389f3a7e3245686bbb7c7317c700c5b9d
Francois Harbec <fharbec@gmail.com>
2012-02-05 20:54:49 -0800

    implement most basic case of multi image upload

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,24 +5,19 @@ class PhotosController < ApplicationController
     @photos = @gallery.photos
   end
   
-  def new
-    @photo = @gallery.photos.new
-  end
-
-# Parameters: {"utf8"=>"✓", "authenticity_token"=>"fdoLXXzi//6k2DwCLBYKvhWHknS2Bsj8bua4VHfCm9s=", "photo"=>{"name"=>"fred", "file"=>#<ActionDispatch::Http::UploadedFile:0x007fbe6666eac8 @original_filename="coffee-ad.jpg", @content_type="image/jpeg", @headers="Content-Disposition: form-data; name=\"photo[file]\"; filename=\"coffee-ad.jpg\"\r\nContent-Type: image/jpeg\r\n", @tempfile=#<File:/var/folders/sb/h48p5_4j7y3_0wd4c52cr7340000gn/T/RackMultipart20120205-78681-g0dio2>>}, "commit"=>"Create Photo", "gallery_id"=>"1"}  
   def create
     @photo = @gallery.photos.new(params[:photo])
     if @photo.save
-      redirect_to @gallery, :notice => 'Photo successfully uploaded'
+      
     else
-      render :new
+      render :json => { "errors" => @photo.errors } 
     end
   end
   
-  def edit
-  end
-  
-  def update
+  def destroy
+    @photo = @gallery.photos.find(params[:id])
+    @photo.destroy
+    render :json => true    
   end
   
   private
@@ -31,3 +26,4 @@ class PhotosController < ApplicationController
     @gallery = Gallery.find(params[:gallery_id])
   end
 end
+
